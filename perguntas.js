922f1775ed621e4ab7c8fe02d311aaf70d7e5ba7
milena <142347289+milenacruz20@users.noreply.github.com>
2024-10-10 16:40:21 -0300

    2

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,28 @@ criaCartao(
     'Qual foi o primeiro longa metragem animado lançado pela Disney?',
     'Branca de Neve e os 7 anões'
 )
+criaCartao(
+    'Filme',
+    'Qual filme animado mostra um grupo de pequenas criaturas amarelas ajudando um vilão em seus planos de roubo de grandes monumentos?',
+    'Meu Malvado Favorito'
+)
+criaCartao(
+    'Filme',
+    'Qual filme animado mostra um ratinho sonhando em ser chef de cozinha em Paris?',
+    'Ratatouille'
+)
+criaCartao(
+    'Filme',
+    'Qual animação mostra a vida secreta dos brinquedos quando humanos não estão por perto?',
+    'Toy Story'
+)
+criaCartao(
+    'Série',
+    'Qual série mostra um grupo de amigos lidando com eventos sobrenaturais em uma pequena cidade nos anos 80?',
+    'Stranger Things'
+)
+criaCartao(
+    'Série',
+    'Qual série acompanha um professor de química que se torna fabricante de metanfetamina após ser diagnosticado com câncer?',
+    'Breaking Bad'
+)
